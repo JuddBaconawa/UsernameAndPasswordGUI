7e5091fa4d02b8cb2c8c434b86039d31e2fdfc76
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-02-27 22:05:52 -0500

    App.java
    comments added

---
+++ UsernameAndPasswordGUI/src/App.java
@@ -0,0 +1,14 @@
+//packages
+
+
+//imports
+
+
+//main class - APP
+public class App {
+
+    //main method - everything is excecuted 
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
