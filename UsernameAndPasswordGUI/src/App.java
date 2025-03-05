cb316e8967958f8fe40bc68f6c9867026762bf23
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-04 21:36:23 -0500

    Update App.java
    comments added to explain the changes from the last update.
    
    passwordLabel, passwordTextBox, and "login" button created - comments added.

--- UsernameAndPasswordGUI/src/App.java
+++ UsernameAndPasswordGUI/src/App.java
@@ -2,10 +2,11 @@
 
 
 //imports
-import javax.swing.JFrame;    //imported to enable formating of the GUI
-import javax.swing.JLabel;    //imported for placement of contents within a GUI window
-import javax.swing.JPanel;    //uimported for the placement of label that can have text on the GUI
-import javax.swing.JTextField; 
+import javax.swing.JButton;
+import javax.swing.JFrame;      //imported to enable formating of the GUI
+import javax.swing.JLabel;      //imported for placement of contents within a GUI window
+import javax.swing.JPanel;      //uimported for the placement of label that can have text on the GUI
+import javax.swing.JTextField;  //JTextField for user to input a typed response
 
 //main class - APP
 public class App {
@@ -19,19 +20,39 @@ public class App {
         JFrame frame = new JFrame();
         frame.setSize(300, 150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.add(panel);
 
         panel.setLayout(null);
 
-        JLabel label = new JLabel("User");
-        label.setBounds(10, 20, 80, 15);
-        panel.add(label);
+        //username label
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
 
-
+        //username input box
         JTextField userText = new JTextField(20);
         userText.setBounds(100, 20, 165, 25);
         panel.add(userText);
+
+        //password label
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        //pasword input box
+        JTextField passwordText = new JTextField(20);
+        passwordText.setBounds(100, 50, 165,25);
+        panel.add(passwordText);
+
+        // JPasswordField passwordText = new JPasswordField();
+        // passwordText.setBounds(10, 50, 165, 25);
+        // panel.add(passwordText);
+
+        //button created for "login"
+        JButton login = new JButton("Login");
+        login.setBounds(90, 80 , 80, 25);
+        panel.add(login);
+
 
         frame.setVisible(true); //setVisible is set true to allow the GUI to be seen when ran
         
