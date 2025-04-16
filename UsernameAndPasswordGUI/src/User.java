65e5c659135c2f6a07d467cbd71dd15aceb98176
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-15 20:24:23 -0400

    Update User.java
    spelling issues plus wrong data type - replaced "String" with int + lowercased "A" on "Address"

--- UsernameAndPasswordGUI/src/User.java
+++ UsernameAndPasswordGUI/src/User.java
@@ -1,10 +1,10 @@
 public class User {
 
 	//attributes of a User class
-	public String userId;
+	public int userId;
 	public String username;
 	public String email;
 	public String phone;
-	public String Address;
+	public String address;
 	public String password;
 }
