6fee1d3b099e52df31eb88d91b47d5fdbbc28194
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-24 19:40:39 -0400

    Update MainFrame.java
    JLabel imported.
    
    Column label added: name, email, phone, address.

--- UsernameAndPasswordGUI/src/MainFrame.java
+++ UsernameAndPasswordGUI/src/MainFrame.java
@@ -3,6 +3,7 @@
 // IMPORTS
 import java.awt.GridLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
@@ -11,6 +12,15 @@ public class MainFrame extends JFrame {
 		/********************** Info Panel ************************/
 		JPanel infoPanel = new JPanel();
 		infoPanel.setLayout(new GridLayout(0, 2, 5, 5));
+		infoPanel.add(new JLabel("Name"));
+		infoPanel.add(new JLabel(user.username));
+		infoPanel.add(new JLabel("Email"));
+		infoPanel.add(new JLabel(user.email));
+		infoPanel.add(new JLabel("Phone"));
+		infoPanel.add(new JLabel(user.phone));
+		infoPanel.add(new JLabel("Address"));
+		infoPanel.add(new JLabel(user.address));
+
 
 
 		
