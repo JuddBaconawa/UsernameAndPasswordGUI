01c0a2a2b314d2673bc17c060191f5278deadd78
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-23 21:06:40 -0400

    Update MainFrame.java
    JPanel created + imports for swing and gridlayout added

--- UsernameAndPasswordGUI/src/MainFrame.java
+++ UsernameAndPasswordGUI/src/MainFrame.java
@@ -1,13 +1,17 @@
 // PACKAGES
 
 // IMPORTS
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-// MainFrame class
 public class MainFrame extends JFrame {
 
 	public void initialize(User user) {
 		/********************** Info Panel ************************/
-		
+		JPanel infoPanel = new JPanel();
+		infoPanel.setLayout(new GridLayout(0, 2, 5, 5));
+
 	}
 
 
